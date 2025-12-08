1325a3d6c87f49f9747588489c8586c9df7976b2
Rami Al Khatib <khatibrami8@gmail.com>
2025-12-08 23:43:55 +0200

    add new role

--- src/pages/Admin/UsersManagement.tsx
+++ src/pages/Admin/UsersManagement.tsx
@@ -261,7 +261,7 @@ export default function UsersManagement() {
                     <div>
                         <Label>Role</Label>
                         <Select
-                            options={[{ value: 'cashier', label: 'cashier' }, { value: 'admin', label: 'admin' }, { value: "gamecashier", label: "game cashier" }, { value: "admin_fnb", label: "admin f&b" }, { value: "chef", label: "chef" }]}
+                            options={[{ value: 'cashier', label: 'cashier' }, { value: 'admin', label: 'admin' }, { value: "gamecashier", label: "game cashier" }, { value: "admin_fnb", label: "admin f&b" }, { value: "chef", label: "chef" }, { value: "bartender", label: "bartender" }]}
                             placeholder="Select a role"
                             defaultValue={roleName}
                             onChange={(v: string | number) => setRoleName(String(v))}
